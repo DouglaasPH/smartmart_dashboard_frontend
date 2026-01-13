87d89c99865d968bd21a91b4cb45ab83658f0b25
Douglas Phelipe <douglasphelipedpss@gmail.com>
2026-01-12 23:27:09 -0300

    feat(products): update products page structure

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,14 +1,55 @@
+// react
+import { useState } from "react";
+
+// components
 import AddNewCategoryForProduct from "./components/addNewCategoryForProduct";
+import CardOfEditProductForProduct from "./components/cardOfEditProductForProduct";
 import SearchAndFilterForProduct from "./components/SearchAndFilterForProduct";
+import TableForProducts from "./components/tableForProducts";
 import TitleAndButtonsForProducts from "./components/titleAndButtonsForProducts";
 
+// types
+import type { Product } from "@/types";
+
 function Products() {
+  const [isEditProduct, setIsEditProduct] = useState(false);
+  const [isAddCategory, setIsAddCategory] = useState(false);
+  const [selectedCategory, setSelectedCategory] = useState("Todas categorias");
+  const [dataForEditProduct, setDataForEditProduct] = useState<Product>({
+    id: 2,
+    name: "LG OLED55C1",
+    description:
+      "55-inch OLED 4K Smart TV with AI ThinQ and G-Sync compatibility",
+    price: "1499.99",
+    category: {
+      id: 1,
+      name: "TVs",
+    },
+    brand: "LG",
+  });
+
   return (
     <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div className="flex items-center justify-between flex-col h-16 mt-20 gap-8">
-        <TitleAndButtonsForProducts />
-        <AddNewCategoryForProduct />
-        <SearchAndFilterForProduct />
+        <TitleAndButtonsForProducts setIsAddCategory={setIsAddCategory} />
+        {isAddCategory ? (
+          <AddNewCategoryForProduct setIsAddCategory={setIsAddCategory} />
+        ) : null}
+        <SearchAndFilterForProduct
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+        <TableForProducts
+          setIsEditProduct={setIsEditProduct}
+          setDataForEditProduct={setDataForEditProduct}
+        />
+        {isEditProduct ? (
+          <CardOfEditProductForProduct
+            data={dataForEditProduct}
+            setData={setDataForEditProduct}
+            setIsEditProduct={setIsEditProduct}
+          />
+        ) : null}
       </div>
     </div>
   );
